f55620d50bafd4487b7bd0d607dac836d5300fd7
trivvet <trivvet@gmail.com>
2018-11-26 18:43:09 +0200

    add content type fields to Comment model

--- src/comments/models.py
+++ src/comments/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from posts.models import Post
@@ -10,12 +12,18 @@ from posts.models import Post
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    post =  models.ForeignKey(Post)
+    # post =  models.ForeignKey(Post)
+
+    content_type = models.ForeignKey(ContentType, 
+        on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
     content = models.TextField(blank=False, null=False)
     timestamp = models.DateField(auto_now_add=True)
 
     def __unicode__(self):
-        return str(u"Comment about {}".format(self.post))
+        return str(u"Comment about {}".format(self.user))
 
     def __str__(self):
-        return str(u"Comment about {}".format(self.post))
+        return str(u"Comment about {}".format(self.user))
